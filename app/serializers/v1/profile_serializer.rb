95e0eb558cee8456e795d8fcd59d5bf14ab4d9ca
Shlomi Zadok <shlomi@ben-hanna.com>
2016-05-31 07:44:58 +0300

    fixes of no start_at error

--- app/serializers/v1/profile_serializer.rb
+++ app/serializers/v1/profile_serializer.rb
@@ -7,7 +7,7 @@ class V1::ProfileSerializer < ActiveModel::Serializer
     attributes :id, :start_time, :active, :organic, :region_name, :vegetables, :hours, :user_id, :start_at, :about
 
     def start_time
-      object.start_at.strftime('%d-%m-%Y')
+      object.start_at.strftime('%d-%m-%Y') if object && object.start_at
     end
   end
 end
